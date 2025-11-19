d0bb4bf31645ca2a9bcf66a52d66e68fc9906514
fdb <fdb@mail.com>
2025-11-19 15:47:25 +0100

    explications BoutonImportant

--- src/main/java/fr/insa/toto/webui/styles/BoutonImportant.java
+++ src/main/java/fr/insa/toto/webui/styles/BoutonImportant.java
@@ -37,7 +37,16 @@ public class BoutonImportant extends Button{
     
     public BoutonImportant(String message) {
         super(message);
+        // fixer la classe permet facilement de modifier la présentation CSS
+        // en modifiant le fichier de style
+        // le fichier de style est défini par l'annotation @Theme de la classe Application
+        // il doit correspondre à un dossier dans src/main/frontend/themes
+        // ce dossier doit contenir un fichier styles.css
         this.setClassName("boutonImportant");
+        // on peut aussi fixer directement certains attributs css :
+        // getStyle permet de récupérer la description css associée à un composant
+        // puis on fait des set pour imiter la syntaxe css clé : valeur
+        // ci dessous, cela correspond à ajouter font-size : 1.5em; au css
         this.getStyle().set("font-size", "1.5em");
     }
     
